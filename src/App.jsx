3325db2dfe81d796df2c3ce0ba4af3e59d0604fb
KT147 <kevin.tammm@gmail.com>
2024-12-25 11:26:23 +0200

    Kodutöö 8

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,45 @@ import Avaleht from './pages/Avaleht';
 import Kontakt from './pages/kontakt';
 import Meist from './pages/Meist';
 import Seaded from './pages/Seaded';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [sisselogitud, muudaSisselogitud] = useState ("ei");
+  const [sonum, muudaSonum] = useState ("");
+  const kasutajanimiRef = useRef ();
+  const paroolRef = useRef ();
+
+  const logiSisse = () => {
+    if (paroolRef.current.value === "123") {
+    muudaSisselogitud ("jah");
+    muudaSonum (kasutajanimiRef.current.value +", oled sisse logitud");
+  } else {
+    muudaSonum ("Vale parool");
+  }
+}
+  const logiValja = () => {
+    muudaSisselogitud ("ei");
+    muudaSonum ("");
+  }
 
 
-  return (
-    <>
+
+    return (
+    <div className='App'>
+
+      <div>{sonum}</div>
+
+      {sisselogitud === "ei" && 
+      <div>
+      <label>Kasutajanimi</label>
+      <input type="text" ref={kasutajanimiRef} /> <br /> <br />
+      <label>Parool</label>
+      <input type="password" ref={paroolRef}  /> <br /><br /> 
+      </div>}
+
+    {sisselogitud === "ei" && <button onClick={logiSisse}>Logi sisse</button>}
+    {sisselogitud === "jah" && <button onClick={logiValja}>Logi välja</button>} <br /> <br />
+
     <Link to = "./avaleht">
         <button>Avaleht</button>
     </Link>
@@ -29,7 +62,7 @@ function App() {
         <Route path="meist" element={< Meist />} />   
         <Route path="seaded" element={< Seaded />} />  
       </Routes>
-    </>
+    </div>
   )
 }
 
